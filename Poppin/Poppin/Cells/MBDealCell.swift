f025e3eda8a5173e95956a5b6647bc092101ef53
Steven Penava <steven.penava@gmail.com>
2018-11-06 15:13:37 -0500

    Added login page UI.

---
+++ Poppin/Poppin/Cells/MBDealCell.swift
@@ -0,0 +1,21 @@
+// File: MBDealCell.swift
+// Purpose: <enter purpose>
+// Date Created: 10/16/18
+// Created By: Steven Penava
+
+import UIKit
+
+class MBDealCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
