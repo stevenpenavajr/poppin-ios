3cebe209479f1cc9ef5f55fe84aa6579e007bf18
Blake Swaidner <blake.swaidner@gmail.com>
2018-11-30 16:46:20 -0500

    Blake - fixing project files

--- Poppin/Poppin/View/DealDetailsTitleCell.swift
+++ Poppin/Poppin/View/DealDetailsTitleCell.swift
@@ -1,21 +1,26 @@
-// File: SpecificDealCell.swift
+// File: DealDetailsTitleCell.swift
 // Purpose: <enter purpose>
 // Date Created: 11/30/18
 // Created By: Steven Penava
 
 import UIKit
 
-class DealDetailsCell: UITableViewCell {
-
+class DealDetailsTitleCell: UITableViewCell {
+    
+    static let height: CGFloat = 100.0
+    
+    @IBOutlet weak var barTitleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        barTitleLabel.font = UIFont.systemFont(ofSize: 14, weight: .heavy)
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
