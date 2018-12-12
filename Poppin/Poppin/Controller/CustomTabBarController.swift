d749014ddb4c79f73de85f83337bc3ee0a0788ad
Steven Penava <steven.penava@gmail.com>
2018-12-11 21:32:14 -0500

    Fixed tab bar height issue, fixed tab bar image resize issue, fixed tab bar selected image issue, added settings icon, added functioning maps to pubmapcell and detailsmapcell.

--- Poppin/Poppin/Controller/CustomTabBarController.swift
+++ Poppin/Poppin/Controller/CustomTabBarController.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    let mapIconGray = UIImage(named: "map-gray")!.resizeImage(size: CGSize(width: 30, height: 30))
+    let mapIconPoppin = UIImage(named: "map-poppin")!.resizeImage(size: CGSize(width: 30, height: 30))
+    
+    let dealsIconGray = UIImage(named: "deals-gray")!.resizeImage(size: CGSize(width: 35, height: 35))
+    let dealsIconPoppin = UIImage(named: "deals-poppin")!.resizeImage(size: CGSize(width: 35, height: 35))
+    
+    let settingsIconGray = UIImage(named: "settings-1-gray")!.resizeImage(size: CGSize(width: 30, height: 30))
+    let settingsIconPoppin = UIImage(named: "settings-1-poppin")!.resizeImage(size: CGSize(width: 30, height: 30))
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func styleTabBar(_ poppinTabBar: UITabBar) {
+        self.tabBar.isUserInteractionEnabled = false
+        self.tabBar.items?[0].image = dealsIconGray.withRenderingMode(.alwaysOriginal)
+        self.tabBar.items?[0].selectedImage = dealsIconPoppin.withRenderingMode(.alwaysOriginal)
+        self.tabBar.items?[0].title = nil
+        self.tabBar.items?[0].imageInsets = UIEdgeInsets(top: 8, left: 0, bottom: -8, right: 0)
+        
+        self.tabBar.items?[1].image = mapIconGray.withRenderingMode(.alwaysOriginal)
+        self.tabBar.items?[1].selectedImage = mapIconPoppin.withRenderingMode(.alwaysOriginal)
+        self.tabBar.items?[1].title = nil
+        self.tabBar.items?[1].imageInsets = UIEdgeInsets(top: 8, left: 0, bottom: -8, right: 0)
+        
+        self.tabBar.items?[2].image = settingsIconGray.withRenderingMode(.alwaysOriginal)
+        self.tabBar.items?[2].selectedImage = settingsIconPoppin.withRenderingMode(.alwaysOriginal)
+        self.tabBar.items?[2].title = nil
+        self.tabBar.items?[2].imageInsets = UIEdgeInsets(top: 8, left: 0, bottom: -8, right: 0)
+    }
+}
+
